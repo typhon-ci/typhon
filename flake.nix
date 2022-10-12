b3bdc97cb8be3eefdd61e71f93ff2bafcdf80f43
Lucas Franceschino <lucas.franceschino@inria.fr>
2022-10-12 15:44:28 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -4,36 +4,70 @@
   inputs = {
     crane.url = "github:ipetkov/crane";
     nixpkgs.follows = "crane/nixpkgs";
-  };
-
-  outputs = { self, crane, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      craneLib = crane.lib.${system};
-      pkgs = import nixpkgs { inherit system; };
-      typhon = craneLib.buildPackage {
-        buildInputs = [ pkgs.sqlite.dev ];
-        src = pkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter = path: type:
-            path == toString ./Cargo.toml || path == toString ./Cargo.lock
-            || pkgs.lib.hasPrefix (toString ./src) path;
-        };
-      };
-      typhonShell = pkgs.mkShell {
-        packages = [ pkgs.diesel-cli pkgs.sqlite pkgs.sqlitebrowser ];
-        DATABASE_URL = "sqlite:typhon.sqlite";
-      };
-    in {
-      packages.${system} = {
-        inherit typhon;
-        default = typhon;
-      };
-      devShells.${system}.default = typhonShell;
-      nixosModules.default = import ./nixos/typhon.nix typhon;
-      checks.${system}.default = import ./nixos/test.nix {
-        inherit system nixpkgs;
-        typhon = self;
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
       };
     };
+  };
+
+  outputs = { self, crane, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        craneLib = crane.lib.${system};
+        rust-wasm = pkgs.rust-bin.stable.latest.default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        craneLibWasm = (crane.mkLib pkgs).overrideToolchain rust-wasm;
+      in rec {
+        packages = {
+          typhon-webapp = craneLibWasm.buildPackage {
+            name = "typhon-webapp";
+            buildInputs = [
+              pkgs.pkg-config pkgs.openssl.dev
+              pkgs.nodePackages.sass
+            ];
+            src = craneLib.cleanCargoSource ./.;
+            cargoBuildCommand = "cargo build -p typhon-webapp";
+          };
+          typhon-server = craneLib.buildPackage {
+            name = "typhon";
+            buildInputs = [ pkgs.sqlite.dev ];
+            src = craneLib.cleanCargoSource ./.;
+          };
+          default = packages.typhon-server;
+        };
+        
+        devShells = {
+          default = devShells.typhon-server;
+          typhon-server = pkgs.mkShell {
+            packages = [
+              pkgs.diesel-cli
+              pkgs.sqlite
+              pkgs.sqlitebrowser
+              pkgs.rustfmt
+            ];
+            inputsFrom = [ packages.typhon-server ];
+            DATABASE_URL = "sqlite:typhon.sqlite";
+          };
+          typhon-webapp = pkgs.mkShell {
+            packages = [ pkgs.rustfmt ];
+            inputsFrom = [ packages.typhon-webapp ];
+          };
+        };
+        
+        nixosModules.default = import ./nixos/typhon.nix packages.typhon-server;
+        # checks.default = import ./nixos/test.nix {
+        #   inherit system nixpkgs;
+        #   typhon = self;
+        # };
+      }
+    );
 }
