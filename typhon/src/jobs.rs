d84b687178599198726984eb8d0a464f452cb679
Paul-Nicolas Madelaine <pnm@pnm.tf>
2022-12-02 08:00:00 +0100

    implement jobs

--- typhon/src/jobs.rs
+++ typhon/src/jobs.rs
@@ -1,16 +1,31 @@
+use crate::actions;
 use crate::connection;
 use crate::error::Error;
 use crate::handles;
 use crate::models::*;
 use crate::responses;
 use crate::schema::builds::dsl::*;
+use crate::schema::evaluations::dsl::*;
 use crate::schema::jobs::dsl::*;
+use crate::BUILDS;
 use crate::JOBS;
 use diesel::prelude::*;
+use serde_json::json;
+use std::path::Path;
 
 impl Job {
+    pub fn build(&self, conn: &mut SqliteConnection) -> Result<Build, Error> {
+        Ok(builds.find(self.job_build).first::<Build>(conn)?)
+    }
+
     pub fn cancel(&self) -> Result<(), Error> {
         todo!()
+    }
+
+    pub fn evaluation(&self, conn: &mut SqliteConnection) -> Result<Evaluation, Error> {
+        Ok(evaluations
+            .find(self.job_evaluation)
+            .first::<Evaluation>(conn)?)
     }
 
     pub fn get(conn: &mut SqliteConnection, job_handle: &handles::Job) -> Result<Self, Error> {
@@ -40,7 +55,68 @@ impl Job {
 
     pub async fn run(self) -> () {
         let id = self.job_id;
-        let task = async move { Err(Error::Todo) };
+        let task = async move {
+            let mut conn = connection().await;
+
+            // abort if actions are not defined
+            let evaluation = self.evaluation(&mut conn)?;
+            let path = match &evaluation.evaluation_actions_path {
+                None => return Ok(()),
+                Some(path) => path,
+            };
+
+            let jobset = evaluation.jobset(&mut conn)?;
+            let project = jobset.project(&mut conn)?;
+
+            drop(conn);
+
+            // run action `begin`
+            if Path::new(&format!("{}/begin", path)).exists() {
+                let input = json!({
+                    "project": project.project_name,
+                    "jobset": jobset.jobset_name,
+                    "evaluation": evaluation.evaluation_num,
+                    "job": self.job_name,
+                    "flake": jobset.jobset_flake,
+                    "locked_flake": evaluation.evaluation_locked_flake,
+                });
+                let _ = actions::run(
+                    &project.project_key,
+                    &format!("{}/begin", path),
+                    &format!("{}/secrets", path),
+                    &input,
+                )
+                .await?;
+            }
+
+            // wait for build
+            BUILDS.get().unwrap().wait(&self.job_build).await;
+            let mut conn = connection().await;
+            let build = self.build(&mut *conn)?;
+            drop(conn);
+
+            // run action `end`
+            if Path::new(&format!("{}/end", path)).exists() {
+                let input = json!({
+                    "project": project.project_name,
+                    "jobset": jobset.jobset_name,
+                    "evaluation": evaluation.evaluation_num,
+                    "job": self.job_name,
+                    "flake": jobset.jobset_flake,
+                    "locked_flake": evaluation.evaluation_locked_flake,
+                    "status": build.build_status,
+                });
+                let _ = actions::run(
+                    &project.project_key,
+                    &format!("{}/end", path),
+                    &format!("{}/secrets", path),
+                    &input,
+                )
+                .await?;
+            }
+
+            Ok::<(), Error>(())
+        };
         let f = move |r| async move {
             let status = match r {
                 Some(Ok(())) => "success",
