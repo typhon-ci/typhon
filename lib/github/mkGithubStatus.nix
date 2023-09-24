4c99d275357de88549755f2aaeb94f97764f0f3d
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-09-24 18:39:08 +0200

    lib: update github status message

--- lib/github/mkGithubStatus.nix
+++ lib/github/mkGithubStatus.nix
@@ -23,19 +23,15 @@ in {
 
           build=$(echo "$input" | jq '.input.build' -r)
           data=$(echo "$input" | jq '.input.data' -r)
-          evaluation=$(echo "$input" | jq '.input.evaluation' -r)
           flake_locked=$(echo "$input" | jq '.input.flake_locked' -r)
           job=$(echo "$input" | jq '.input.job' -r)
-          jobset=$(echo "$input" | jq '.input.jobset' -r)
-          project=$(echo "$input" | jq '.input.project' -r)
           status=$(echo "$input" | jq '.input.status' -r)
 
           token=$(echo "$input" | jq '.secrets.github_token' -r)
 
           rev=$(echo "$flake_locked" | sed 's/github:.*\/.*\/\(.*\)/\1/')
           target_url="$(echo "$data" | jq '.url' -r)/builds/$build"
-          context="Typhon: $job"
-          description="$project:$jobset:$evaluation:$job"
+          context="Typhon: ${system} / $job"
           case $status in
             "error")
               state="failure"
@@ -56,7 +52,7 @@ in {
             -H "Accept: application/vnd.github+json" \
             -H "Authorization: Bearer $token" \
             "https://api.github.com/repos/${owner}/${repo}/statuses/$rev" \
-            -d "{\"state\":\"$state\",\"target_url\":\"$target_url\",\"description\":\"$description\",\"context\":\"$context\"}" \
+            -d "{\"state\":\"$state\",\"target_url\":\"$target_url\",\"description\":null,\"context\":\"$context\"}" \
             -k >&2
         '';
       });
