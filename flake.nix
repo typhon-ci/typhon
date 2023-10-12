56f6686a63b574b254dc41ef81f5877b0474b39c
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-12 18:59:23 +0200

    nix: override dependencies of dependencies

--- flake.nix
+++ flake.nix
@@ -11,9 +11,19 @@
 
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
-    crane.url = "github:ipetkov/crane";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.flake-compat.follows = "flake-compat";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: import ./nix/outputs.nix {sources = inputs;};
