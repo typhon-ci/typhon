5d7f5bbdf60a81950a75ec38ed5f4bf2bdb3ce3e
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-17 14:31:55 +0200

    node2nix: use lock file for regeneration

--- typhon-webapp/npm-nix/regenerate.sh
+++ typhon-webapp/npm-nix/regenerate.sh
@@ -1,2 +1,2 @@
-nix run 'nixpkgs#nodePackages.node2nix' -- --input ../package.json
+nix run 'nixpkgs#nodePackages.node2nix' -- --input ../package.json -l ../package-lock.json
 nix run 'nixpkgs#alejandra' -- .
