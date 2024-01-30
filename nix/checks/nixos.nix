b5884cb521b1c483ff4c3b98cd08928ebd2f16d5
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-01-30 01:37:07 +0100

    nix: fix nixos test

--- nix/checks/nixos.nix
+++ nix/checks/nixos.nix
@@ -15,7 +15,6 @@ pkgs.testers.nixosTest ({pkgs, ...}: {
       services.typhon = {
         enable = true;
         hashedPassword = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
-        domain = "127.0.0.1";
       };
     };
   };
