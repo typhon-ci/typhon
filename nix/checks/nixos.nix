5b6182f0ab5f64a6dec5a5ea0aacd0066078c702
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-02 11:16:52 +0100

    nix: fix nixos test

--- nix/checks/nixos.nix
+++ nix/checks/nixos.nix
@@ -16,6 +16,15 @@ pkgs.testers.nixosTest ({pkgs, ...}: {
         enable = true;
         hashedPassword = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
       };
+      services.nginx = {
+        enable = true;
+        virtualHosts."example.com" = {
+          locations."/" = {
+            proxyPass = "http://localhost:3000";
+            recommendedProxySettings = true;
+          };
+        };
+      };
     };
   };
 
