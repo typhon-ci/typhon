a13c2b44dfd97428b723249f23fc2d95942da87e
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-19 12:05:53 +0100

    add support for gh-pages

---
+++ nix/lib/github/mkPages.nix
@@ -0,0 +1,23 @@
+_: lib: {
+  mkPages = {
+    owner,
+    repo,
+    jobset ? "main",
+    job,
+    system ? "x86_64-linux",
+    branch ? "gh-pages",
+  }:
+    lib.compose.match [
+      {
+        inherit jobset job system;
+        action = lib.github.mkPush {
+          inherit owner repo branch;
+        };
+      }
+      {
+        action = lib.builders.mkActionScript {
+          mkScript = system: ''echo "Nothing to do" >&2'';
+        };
+      }
+    ];
+}
---
+++ nix/lib/github/mkPush.nix
@@ -0,0 +1,39 @@
+utils: lib: {
+  mkPush = {
+    owner,
+    repo,
+    branch,
+  }:
+    lib.builders.mkActionScript {
+      mkPath = system: let
+        pkgs = utils.pkgs.${system};
+      in [pkgs.jq pkgs.git];
+      mkScript = system: ''
+        stdin=$(cat)
+
+        input=$(echo "$stdin" | jq -r '.input | ${utils.jqJsonToBashArray}')
+        declare -A input="($input)"
+
+        [ "''${input[status]}" == "success" ] || { echo "Unsuccessful build: nothing to push" >&2; exit 0; }
+
+        token=$(echo "$stdin" | jq -r '.secrets.github_token')
+
+        cp -r "''${input[out]}" out
+        chmod -R +w out
+        cd out
+
+        git init
+        git config user.email "typhon@typhon-ci.org"
+        git config user.name "Typhon"
+
+        git checkout -b ${branch}
+        git add .
+        export GIT_AUTHOR_DATE="1970-01-01T00:00:00+0000"
+        export GIT_COMMITTER_DATE="1970-01-01T00:00:00+0000"
+        git commit -m "''${input[out]}"
+
+        git remote add origin "https://$token@github.com/${owner}/${repo}"
+        git push -f -u origin ${branch}
+      '';
+    };
+}
