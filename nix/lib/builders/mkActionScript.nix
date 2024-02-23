68a5f0933176160f95ef3858664da5cf0312aa74
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-23 12:05:56 +0100

    Tighten the actions' sandbox (#36)
    - unset environment variables
    - unmount `/etc`
    - fix actions accordingly
    
    see https://github.com/NixOS/nix/issues/8074 about why `$HOME` is now
    set before calling Nix from an action

--- nix/lib/builders/mkActionScript.nix
+++ nix/lib/builders/mkActionScript.nix
@@ -6,7 +6,7 @@ utils: lib: {
     lib.eachSystem (system:
       utils.pkgs.${system}.writeShellApplication {
         name = "action";
-        runtimeInputs = mkPath system;
+        runtimeInputs = [utils.pkgs.${system}.coreutils] ++ mkPath system;
         text = mkScript system;
         excludeShellChecks = ["SC2015"];
       });
