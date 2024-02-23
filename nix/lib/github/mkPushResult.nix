f4bafa37cc195831d7ed6f52ef69b0de5b880b9f
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-02-23 13:56:41 +0100

    Add support for GitHub pages (#35)
    - add an action to push build results to GitHub
    - add an action to deploy a job to GitHub pages

---
+++ nix/lib/github/mkPushResult.nix
@@ -0,0 +1,42 @@
+utils: lib: {
+  mkPushResult = {
+    owner,
+    repo,
+    branch,
+    patches ? [],
+  }:
+    lib.builders.mkActionScript {
+      mkPath = system: let
+        pkgs = utils.pkgs.${system};
+      in [pkgs.jq pkgs.git];
+      mkScript = system: ''
+        stdin=$(cat)
+
+        input=$(echo "$stdin" | jq -r '.input | ${utils.jqJsonToBashArray}')
+        declare -A input="($input)"
+
+        [ "''${input[status]}" == "success" ] || { echo "Unsuccessful build: nothing to push" >&2; exit 0; }
+
+        token=$(echo "$stdin" | jq -r '.secrets.github_token')
+
+        cp -r "''${input[out]}" out
+        chmod -R +w out
+        cd out
+
+        git init
+        ${lib.git.config}
+        git checkout -b ${branch}
+
+        git add .
+        git commit -m "''${input[out]}"
+
+        ${utils.lib.concatMapStrings (patch: ''
+            git am < ${patch system}
+          '')
+          patches}
+
+        git remote add origin "https://$token@github.com/${owner}/${repo}"
+        git push -f -u origin ${branch}
+      '';
+    };
+}
