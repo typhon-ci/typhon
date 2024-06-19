f2b74f697597cb3ccbb3a709bd138abf7a2fe9ee
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-06-19 23:33:10 +0200

    speed-up nix build

--- nix/packages/typhon.nix
+++ nix/packages/typhon.nix
@@ -19,7 +19,34 @@ let
     ];
   };
 
-  cargoArtifacts = craneLib.buildDepsOnly args;
+  leptos-toml = pkgs.writeText "leptos.toml" ''
+    [[workspace.metadata.leptos]]
+    name = "typhon"
+    bin-package = "typhon"
+    lib-package = "typhon-webapp"
+    lib-features = ["hydrate"]
+  '';
+
+  typhon-main = pkgs.writeText "main.rs" ''
+    fn main() {}
+  '';
+
+  cargoArtifacts = craneLib.buildDepsOnly (
+    args
+    // {
+      buildPhaseCargoCommand = ''
+        cat ${leptos-toml} >> Cargo.toml
+        cat ${typhon-main} >> typhon/src/main.rs
+        rm -r typhon/src/bin
+        cargo check --locked
+        cargo leptos build --release -vvv
+      '';
+      nativeBuildInputs = [
+        pkgs.binaryen
+        pkgs.cargo-leptos
+      ];
+    }
+  );
 
   nodeDependencies = (import ../npm-nix { inherit system pkgs; }).nodeDependencies;
 in
@@ -38,10 +65,11 @@ craneLib.buildPackage (
       mkdir -p $out/bin
       cp target/release/typhon $out/bin/
       cp -r target/site $out/bin/
+      cp -r ${nodeDependencies}/lib/node_modules $out/bin/site
+      chmod +w -R $out/bin/site/node_modules
       wrapProgram $out/bin/typhon --set LEPTOS_SITE_ROOT $out/bin/site
     '';
     TYPHON_FLAKE = ../../typhon-flake;
     doNotLinkInheritedArtifacts = true;
-    preFixup = "cp -r ${nodeDependencies}/lib/node_modules $out/bin/site";
   }
 )
