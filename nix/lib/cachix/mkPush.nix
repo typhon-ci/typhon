199034c3d85941d3db12e00c716451dfe8b38bbf
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-09-19 01:44:39 +0200

    Fix `lib.cachix.mkPush` (#63)

--- nix/lib/cachix/mkPush.nix
+++ nix/lib/cachix/mkPush.nix
@@ -12,7 +12,10 @@ _: lib: {
           stdin=$(cat)
           path=$(echo "$stdin" | jq -r '.input.out')
           CACHIX_AUTH_TOKEN=$(echo "$stdin" | jq -r '.secrets.cachix_token')
+          HOME=$(pwd)
           export CACHIX_AUTH_TOKEN
+          export HOME
+          export SYSTEM_CERTIFICATE_PATH="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
           cachix push ${name} "$path" >&2
         '';
       }
