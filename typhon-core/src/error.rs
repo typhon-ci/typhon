typhon-ci/typhon

commit 960006f2e8509492672d91c8167ab7fa9cd94213

Use `derive_more::Display` to reduce boilerplate (#67)

typhon-core/src/error.rs
@@ -3,30 +3,59 @@ use crate::handles;
 use crate::nix;
 use crate::task_manager;
 
-#[derive(Debug)]
+#[derive(Debug, derive_more::Display)]
 pub enum Error {
+    #[display("Access denied")]
     AccessDenied,
+    #[display("Action {_0} encountered an error")]
     ActionError(actions::Error),
+    #[display("Action {_0} was not found")]
     ActionNotFound(handles::Action),
+    #[display("Build {_0} was not found")]
     BuildNotFound(handles::Build),
+    #[display("Run {_0} was not found")]
     RunNotFound(handles::Run),
+    #[display("Bad project declaration")]
     BadProjectDecl,
+    #[display("Bad jobset declaration: {_0}")]
     BadJobsetDecl(String),
+    #[display("Evaluation {_0} was not found")]
     EvaluationNotFound(handles::Evaluation),
+    #[display("Illegal project handle: {_0}")]
     IllegalProjectHandle(handles::Project),
+    #[display("Job {_0} is already running")]
     JobAlreadyRunning(handles::Job),
+    #[display("Job {_0} was not found")]
     JobNotFound(handles::Job),
+    #[display("Jobset {_0} was not found")]
     JobsetNotFound(handles::Jobset),
+    #[display("Log {_0} was not found")]
     LogNotFound(handles::Log),
+    #[display("Nix error: {_0}")]
     NixError(nix::Error),
+    #[display("Project {_0} already exists")]
     ProjectAlreadyExists(handles::Project),
+    #[display("Project {_0} was not found")]
     ProjectNotFound(handles::Project),
+    #[display("ToDo")]
     Todo,
+    #[display("Unexpected database error: {_0}")]
     UnexpectedDatabaseError(diesel::result::Error),
+    #[display("Unexpected time error: {_0}")]
     UnexpectedTimeError(time::error::ComponentRange),
+    #[display("Failed to log in")]
     LoginError,
+    #[display("Task error: {_0}")]
     TaskError(task_manager::Error),
+    #[display("{}", display_webhook_failure(_0))]
     WebhookFailure(Option<String>),
+}
+
+fn display_webhook_failure(output: &Option<String>) -> &str {
+    match output {
+        Some(stdout) => concat!("Bad webhook output: {stdout}"),
+        None => "Webhook failure",
+    }
 }
 
 impl Error {
@@ -39,51 +68,6 @@ impl Error {
             | TaskError(_)
             | Todo => true,
             _ => false,
-        }
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use Error::*;
-        match self {
-            AccessDenied => write!(f, "Access denied"),
-            ActionError(e) => write!(f, "Action error: {}", e),
-            ActionNotFound(h) => write!(f, "Action not found: {}", h),
-            BuildNotFound(h) => write!(f, "Build not found: {}", h),
-            RunNotFound(h) => write!(f, "Run not found: {}", h),
-            BadProjectDecl => write!(f, "Bad project declaration"),
-            BadJobsetDecl(s) => write!(f, "Bad jobset declaration: {}", s),
-            IllegalProjectHandle(handle) => {
-                write!(f, "The project name [{}] is illegal. Legal project names are sequences of alphanumerical characters that may contain dashes [-] or underscores [_].", handle.name)
-            }
-            JobAlreadyRunning(job_handle) => {
-                write!(f, "Job {} is already running", job_handle)
-            }
-            JobNotFound(job_handle) => {
-                write!(f, "Job {} not found", job_handle)
-            }
-            JobsetNotFound(jobset_handle) => {
-                write!(f, "Jobset {} not found", jobset_handle)
-            }
-            LogNotFound(log_handle) => {
-                write!(f, "Log {} not found", log_handle)
-            }
-            EvaluationNotFound(evaluation_handle) => {
-                write!(f, "Evaluation {} not found", evaluation_handle)
-            }
-            ProjectAlreadyExists(project_handle) => {
-                write!(f, "Project {} already exists", project_handle)
-            }
-            ProjectNotFound(project_handle) => write!(f, "Project {} not found", project_handle),
-            NixError(e) => write!(f, "Nix error: {}", e),
-            LoginError => write!(f, "Login error"),
-            Todo => write!(f, "Unspecified error"),
-            UnexpectedDatabaseError(e) => write!(f, "Database error: {}", e),
-            UnexpectedTimeError(e) => write!(f, "Time error: {}", e),
-            TaskError(e) => write!(f, "Task error: {}", e),
-            WebhookFailure(None) => write!(f, "Webhook failure"),
-            WebhookFailure(Some(output)) => write!(f, "Bad webhook output: {}", output),
         }
     }
 }

Cargo.toml
@@ -35,6 +35,7 @@ async-stream = "0.3"
 async-trait = "0.1"
 clap = { version = "4.5", features = ["derive", "env"] }
 console_error_panic_hook = "0.1"
+derive_more = { version = "1.0", features = ["display"] }
 diesel = { version = "2.2", features = ["sqlite", "returning_clauses_for_sqlite_3_35", "r2d2"] }
 diesel_migrations = "2.2"
 either = "1.13"

typhon-core/Cargo.toml
@@ -10,6 +10,7 @@ argon2.workspace = true
 async-recursion.workspace = true
 async-stream.workspace = true
 async-trait.workspace = true
+derive_more.workspace = true
 diesel.workspace = true
 diesel_migrations.workspace = true
 ext-trait.workspace = true

typhon-core/src/actions.rs
@@ -17,30 +17,22 @@ use std::process::Stdio;
 use std::str::FromStr;
 use tokio::sync::mpsc;
 
-#[derive(Debug)]
+#[derive(Debug, derive_more::Display)]
 pub enum Error {
+    #[display("Invalid key")]
     InvalidKey,
+    #[display("Wrong secrets format")]
     InvalidSecrets,
+    #[display("Outputted non-UTF8 characters")]
     NonUtf8,
+    #[display("Action script not found")]
     ScriptNotFound,
+    #[display("Secrets file not found")]
     SecretsNotFound,
+    #[display("Secrets file encrypted with wrong key")]
     WrongRecipient,
+    #[display("Unexpected error")]
     Unexpected,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        use Error::*;
-        match self {
-            InvalidKey => write!(f, "Invalid key"),
-            InvalidSecrets => write!(f, "Wrong secrets format"),
-            NonUtf8 => write!(f, "Action outputted non-UTF8 characters"),
-            ScriptNotFound => write!(f, "Action script not found"),
-            SecretsNotFound => write!(f, "Secrets file not found"),
-            WrongRecipient => write!(f, "Secrets file uncrypted with wrong key"),
-            Unexpected => write!(f, "Unexpected error"),
-        }
-    }
 }
 
 mod sandboxed_command {

typhon-core/src/nix.rs
@@ -18,7 +18,8 @@ pub enum Expr {
     Path(String),
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, derive_more::Display)]
+#[display("Evaluation error: {self:#?}")]
 pub enum Error {
     SerdeJson(String), // serde_json::Error is not Clone
     UnexpectedOutput {
@@ -32,12 +33,6 @@ pub enum Error {
     },
     ExpectedDrvGotAttrset(Expr),
     BuildFailed,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Evaluation error: {:#?}", self)
-    }
 }
 
 impl From<serde_json::Error> for Error {
@@ -238,15 +233,10 @@ pub async fn derivation_json(expr: &Expr) -> Result<serde_json::Value, Error> {
     Ok(serde_json::from_str(&cmd.sync_stdout().await?).unwrap())
 }
 
-#[derive(Clone, Debug, PartialEq, Hash, Eq)]
+#[derive(Clone, PartialEq, Eq, Hash, Debug, derive_more::Display)]
+#[display("{path}")]
 pub struct DrvPath {
     path: String,
-}
-
-impl std::fmt::Display for DrvPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "{}", self.path)
-    }
 }
 
 impl DrvPath {

typhon-core/src/task_manager.rs
@@ -5,15 +5,10 @@ use tokio::sync::watch;
 use std::collections::HashMap;
 use std::future::Future;
 
-#[derive(Debug)]
+#[derive(Debug, derive_more::Display)]
 pub enum Error {
+    #[display("Task manager is shutting down")]
     ShuttingDown,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Task manager is shutting down")
-    }
 }
 
 enum Msg<Id> {

typhon-types/Cargo.toml
@@ -4,6 +4,7 @@ version.workspace = true
 edition.workspace = true
 
 [dependencies]
+derive_more.workspace = true
 lazy_static.workspace = true
 regex.workspace = true
 serde.workspace = true

typhon-types/src/lib.rs
@@ -233,15 +233,21 @@ pub mod requests {
         use serde::{Deserialize, Serialize};
         use uuid::Uuid;
 
-        #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+        #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, derive_more::Display)]
         #[serde(rename_all = "lowercase")]
         #[serde(tag = "type")]
         pub enum Kind {
+            #[display("projects")]
             Projects,
+            #[display("jobsets")]
             Jobsets(Jobset),
+            #[display("evaluations")]
             Evaluations(Evaluation),
+            #[display("builds")]
             Builds(Build),
+            #[display("actions")]
             Actions(Action),
+            #[display("runs")]
             Runs(Run),
         }
 
@@ -251,20 +257,6 @@ pub mod requests {
             pub offset: u32,
             #[serde(flatten)]
             pub kind: Kind,
-        }
-
-        impl std::fmt::Display for Kind {
-            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-                let name = match self {
-                    Self::Projects => "projects",
-                    Self::Jobsets(..) => "jobsets",
-                    Self::Evaluations(..) => "evaluations",
-                    Self::Builds(..) => "builds",
-                    Self::Actions(..) => "actions",
-                    Self::Runs(..) => "runs",
-                };
-                write!(f, "{name}")
-            }
         }
 
         #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, Default)]
@@ -360,47 +352,30 @@ pub mod requests {
         Info,
     }
 
-    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, derive_more::Display)]
     pub enum Request {
+        #[display("Search through {}", _0.kind)]
         Search(search::Request),
+        #[display("Create project {name} with url {} (flake: {})", decl.url, decl.flake)]
         CreateProject { name: String, decl: ProjectDecl },
+        #[display("{_1:?} for project {_0}")]
         Project(handles::Project, Project),
+        #[display("{_1:?} for jobset {_0}")]
         Jobset(handles::Jobset, Jobset),
+        #[display("{_1:?} for evaluation {_0}")]
         Evaluation(handles::Evaluation, Evaluation),
+        #[display("{_1:?} for job {_0}")]
         Job(handles::Job, Job),
+        #[display("{_1:?} for build {_0}")]
         Build(handles::Build, Build),
+        #[display("{_1:?} for action {_0}")]
         Action(handles::Action, Action),
+        #[display("{_1:?} for run {_0}")]
         Run(handles::Run, Run),
+        #[display("Log in")]
         Login { password: String },
+        #[display("Get current user")]
         User,
-    }
-
-    impl std::fmt::Display for Request {
-        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            match self {
-                Request::Search(search::Request { kind, .. }) => {
-                    write!(f, "Search through {kind}")
-                }
-                Request::CreateProject { name, decl } => {
-                    write!(
-                        f,
-                        "Create{} project {} with url {}",
-                        if !decl.flake { " legacy" } else { "" },
-                        name,
-                        decl.url
-                    )
-                }
-                Request::Project(h, req) => write!(f, "{:?} for project {}", req, h),
-                Request::Jobset(h, req) => write!(f, "{:?} for jobset {}", req, h),
-                Request::Evaluation(h, req) => write!(f, "{:?} for evaluation {}", req, h),
-                Request::Job(h, req) => write!(f, "{:?} for job {}", req, h),
-                Request::Build(h, req) => write!(f, "{:?} for build {}", req, h),
-                Request::Action(h, req) => write!(f, "{:?} for action {}", req, h),
-                Request::Run(h, req) => write!(f, "{:?} for run {}", req, h),
-                Request::Login { .. } => write!(f, "Log in"),
-                Request::User => write!(f, "Get current user"),
-            }
-        }
     }
 }
 
@@ -534,21 +509,14 @@ pub mod responses {
         User(Option<data::User>),
     }
 
-    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+    #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, derive_more::Display)]
     pub enum ResponseError {
+        #[display("Bad request: {_0}")]
         BadRequest(String),
+        #[display("Internal server error")]
         InternalError,
+        #[display("Resource not found: {_0}")]
         ResourceNotFound(String),
-    }
-
-    impl std::fmt::Display for ResponseError {
-        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            match self {
-                ResponseError::BadRequest(e) => write!(f, "Bad request: {}", e),
-                ResponseError::InternalError => write!(f, "Internal server error"),
-                ResponseError::ResourceNotFound(e) => write!(f, "Resource not found: {}", e),
-            }
-        }
     }
 }
 

commit 7851ecc8712184ba8594ee055f193437e14119bc

Fix a careless mistake (#74)
...which was introduced in `960006f` (#67)

typhon-core/src/error.rs
@@ -51,10 +51,10 @@ pub enum Error {
     WebhookFailure(Option<String>),
 }
 
-fn display_webhook_failure(output: &Option<String>) -> &str {
+fn display_webhook_failure(output: &Option<String>) -> String {
     match output {
-        Some(stdout) => concat!("Bad webhook output: {stdout}"),
-        None => "Webhook failure",
+        Some(stdout) => format!("Bad webhook output: {stdout}"),
+        None => "Webhook failure".to_string(),
     }
 }
 