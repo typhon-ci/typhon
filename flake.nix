e4f11b3d7c47c3a584fe565637bcc6e50f3735da
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-10-31 12:21:33 +0100

    Drop flake schemas (#75)

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs = {
     flake-compat.url = "https://git.lix.systems/api/v1/repos/lix-project/flake-compat/archive/main.tar.gz";
 
-    flake-schemas.url = "github:determinatesystems/flake-schemas";
-
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     crane.url = "github:ipetkov/crane";
